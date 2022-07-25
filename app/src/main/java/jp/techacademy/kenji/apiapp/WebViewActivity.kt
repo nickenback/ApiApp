1f42e7d813029708b7330180cc6d983da7277d53
nickenback <k-fujiwara@tasuta.com>
2022-07-25 16:49:32 +0900

    課題提出

--- app/src/main/java/jp/techacademy/kenji/apiapp/WebViewActivity.kt
+++ app/src/main/java/jp/techacademy/kenji/apiapp/WebViewActivity.kt
@@ -74,7 +74,7 @@ class WebViewActivity : AppCompatActivity() {
 
     companion object {
         private const val KEY_FAVS = "key_FAVS"
-        
+
         fun start(activity: Activity, shop: FavoriteShop) {
 
             activity.startActivity(
